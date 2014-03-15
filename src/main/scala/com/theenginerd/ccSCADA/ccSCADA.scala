b6ce4edadc43c3bd902aea265ce23eba3fe9f64e
Mr. Byte <bytemr@gmail.com>
2014-03-15 11:49:43 -0600

    Initial setup of the mod and required API.

---
+++ src/main/scala/com/theenginerd/ccSCADA/ccSCADA.scala
@@ -0,0 +1,28 @@
+/*
+ * Copyright 2014 Joshua R. Rodgers
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ========================================================================
+ */
+
+package com.theenginerd.ccSCADA
+
+import cpw.mods.fml.common.Mod
+import cpw.mods.fml.common.network.NetworkMod
+
+@Mod(name = NAME, modid = ID, version=VERSION, dependencies=DEPENDENCIES, modLanguage="scala")
+@NetworkMod(clientSideRequired = true, serverSideRequired = false)
+object ccSCADA
+{
+
+}
